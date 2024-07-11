565816979eab98af161c03f4df7a5c089e16f9ff
Iuliia <markelova.uv@gmail.com>
2024-07-11 20:15:10 +0300

    extracted scanner from dot constructor

--- src/main/java/cy/markelova/coordinates/entity/Dot.java
+++ src/main/java/cy/markelova/coordinates/entity/Dot.java
@@ -1,22 +1,17 @@
 package cy.markelova.coordinates.entity;
 
-import java.util.Scanner;
-
 public class Dot {
     private final int COORDINATE_X;
     private final int COORDINATE_Y;
 
-    public Dot() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the coordinates X and Y of the dot separated by a space: ");
-        this.COORDINATE_X = scanner.nextInt();
-        this.COORDINATE_Y = scanner.nextInt();
+    public Dot(int x, int y) {
+        this.COORDINATE_X = x;
+        this.COORDINATE_Y = y;
     }
 
     public int getCoordinateX() {
         return COORDINATE_X;
     }
-
 
     public int getCoordinateY() {
         return COORDINATE_Y;
