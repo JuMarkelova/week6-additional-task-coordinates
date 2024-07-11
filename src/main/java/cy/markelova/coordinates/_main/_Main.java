ade94eec2e776dcb2b7e00e0ab335268c46d0bbf
Iuliia <markelova.uv@gmail.com>
2024-07-11 20:23:53 +0300

    removed Runner

--- src/main/java/cy/markelova/coordinates/_main/_Main.java
+++ src/main/java/cy/markelova/coordinates/_main/_Main.java
@@ -1,11 +1,24 @@
 package cy.markelova.coordinates._main;
 
-import cy.markelova.coordinates.entity.Runner;
+import cy.markelova.coordinates.entity.Dot;
+import cy.markelova.coordinates.util.Actions;
+
+import java.util.Scanner;
 
 public class _Main {
 
     public static void main(String[] args) {
-        Runner runner = new Runner();
-        runner.runMath();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the coordinates X and Y of the first dot separated by a space: ");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        System.out.println("Enter the coordinates X and Y of the second dot separated by a space: ");
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        Dot dotFirst = new Dot(x1, y1);
+        Dot dotSecond = new Dot(x2, y2);
+        System.out.printf("The length of the segment is equal to %.2f.\nThe result is rounded to decimal places.",
+                Actions.calculateLengthSegment(dotFirst, dotSecond));
+        scanner.close();
     }
 }
